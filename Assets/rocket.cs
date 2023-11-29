6123e014b08f3837439e4f97698e7eae917c9631
susmita2022khatun <120239539+susmita2022khatun@users.noreply.github.com>
2023-11-29 14:23:24 +0530

    Add files via upload

---
+++ Assets/rocket.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rocket : MonoBehaviour {
+	Rigidbody rigidBody;
+	AudioSource audioSource;
+	[SerializeField] float rcsThrust = 100f;
+	[SerializeField] float mainThrust = 100f;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Thrust();
+		Rotate();
+	}
+	void OnCollisionEnter(Collision collision){
+		switch (collision.gameObject.tag){
+			case "Friendly":
+				print("OK");
+	        	break;
+			case "Fuel":
+				print("Fuel");
+				break;
+			default:
+				print("dead");
+				break;
+		}
+	}
+	private void Rotate(){
+		rigidBody.freezeRotation = true;
+		float rotationThisFrame = rcsThrust * Time.deltaTime; 
+		if(Input.GetKey(KeyCode.A)){
+			transform.Rotate(Vector3.forward * rotationThisFrame);
+		}
+		else if(Input.GetKey(KeyCode.D)){
+			transform.Rotate(-Vector3.forward* rotationThisFrame);
+		}
+		rigidBody.freezeRotation = false;
+	}
+	private void Thrust(){
+		if(Input.GetKey(KeyCode.Space)){
+			rigidBody.AddRelativeForce(Vector3.up*mainThrust);
+			if(!audioSource.isPlaying){
+				audioSource.Play();
+			}
+		}
+		else{
+				audioSource.Pause();
+			}
+	}
+}
